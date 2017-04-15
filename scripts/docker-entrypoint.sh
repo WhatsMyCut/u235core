9ceead2c9bf272fd46290bfb636cd3dbc902c109
Mike Taylor <mike@whatsmycut.com>
2017-04-15 10:01:28 -0500

    export path

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Get dotenv file from unicreds and export it as environment vars
-export $(/usr/local/bin/unicreds -t U235Config${ENVIRONMENT} -k alias/U235Key${ENVIRONMENT} -r us-east-1 get u235core-env)
+#export $(/usr/local/bin/unicreds -t U235Config${ENVIRONMENT} -k alias/U235Key${ENVIRONMENT} -r us-east-1 get u235core-env)
+export $PATH
 eval "$@"
